40dec3a9dfc401279bb335600f278effe5aab60a
AJ Fernández <agustinjfd@gmail.com>
2019-04-16 12:45:39 +0200

    added new functions

--- wpAnalyzer_v2.1.3.py
+++ wpAnalyzer_v2.1.3.py
@@ -1,12 +1,15 @@
-from urllib.request import urlopen as urlReq, urlretrieve as urlDownload, Request 
+from urllib.request import urlopen as urlReq, urlretrieve as urlDownload, Request
 from pathlib import Path
-import hashlib
+import hashlib, traceback
 import sys
 import re
 import json
 import shutil
 import os
 import yara
+
+ver = ""
+badHashFiles = []
 
 
 def banner():
@@ -26,6 +29,11 @@ def Target_Files(pwd):
     return(allFiles)
 
 
+def Answer_Ver():
+    global ver
+    ver = input("\n\nWhat is the wordpress version of target site?: ")
+
+
 def Yara_Checker():
     if Path("./yaraRules").exists:
         return True
@@ -34,8 +42,8 @@ def Yara_Checker():
 
 
 def Core_Analysis(paths):
+    global badHashFiles
     print("\n_______________________CORE FILES HASHES ANALYSIS_______________________\n")
-    ver = input("What is the wordpress version of target site?: ")
     print("\n")
     fullBadFiles = [None]
     apiHashes = Pull_Json(ver)
@@ -48,7 +56,7 @@ def Core_Analysis(paths):
     print("________________________________________________________________________\n")
     if fullBadFiles == [None]:
         print("All analyzed files keep the correct hashes\n")
-    return(fullBadFiles, ver)
+    badHashFiles = fullBadFiles
 
 
 def Compare_Hash(apiHashes, chkSum, pthFile, ver):
@@ -58,14 +66,14 @@ def Compare_Hash(apiHashes, chkSum, pthFile, ver):
         return(pthFile)
 
 
-def WP_CoreFiles_Replace(listBadFiles, ver):
+def WP_CoreFiles_Replace():
     recovery = input(
         "Do you want download and replace the core files with invalid hashes?: (y/n) ").lower()
     if recovery == "y":
         print("\nBegin the replacing actions:")
         print("\n\tDownload Wordpress Core:\n\t")
         NewCoreFolder = Download_File(ver)
-        for targetFile in listBadFiles:
+        for targetFile in badHashFiles:
             src = NewCoreFolder + "/wordpress/" + str(targetFile)
             dst = targetFile
             if targetFile == None:
@@ -134,9 +142,22 @@ def Find_Suspect_Code(paths):
 
 
 def Yara_Malware_Analysis():
-    Yara_Rules_Adaptation()
-    indexRules = yara.compile(
-        filepath="./yaraRules/rules-master/malware_index.yar")
+    # A recursive function needed here; some rules included in index rules file are broken so we need caught this 
+    # exception, parse the message for match the malformed rule name and remove it, first from index file and the file itself later. 
+    try:
+        indexRules = yara.compile(filepath="./yaraRules/rules-master/malware_index.yar")
+    except yara.SyntaxError:
+        #Extract malformed rule filename from "sys.exc_info(): return information about the exception that is being handled" 
+        excFile = str(sys.exc_info()[1]).split("(")[0]
+        print("\nError loading: " + "\x1b[1;31m" + Path(excFile).name + "\x1b[0;m" + " rule, it will be removed")
+        modContent = []
+        with open("./yaraRules/rules-master/malware_index.yar", "r", encoding="ISO-8859-1") as indexFile:
+            # List comprehensions are shorter TODO: Change all modification files functions on this way
+            modContent += [line for line in indexFile.readlines() if Path(excFile).name not in line]
+        with open("./yaraRules/rules-master/malware_index.yar", "w", encoding="ISO-8859-1") as indexFile:
+            indexFile.write("\n".join(modContent))
+        return Yara_Malware_Analysis()
+        
     excludeSuffix = [".yar", ".yara"]
     allFiles = Target_Files(".")
     print("\n\n_______________________YARA MALWARE ANALYSIS_______________________\n")
@@ -172,7 +193,11 @@ def Yara_Webshells_Analysis():
 
 
 def Yara_Personal_Rules():
-    rule = yara.compile(filepath="./lepetitpol.yar")
+    if not Path("./personalRule.yar").is_file():
+        print(
+            "\x1b[1;31m" + "\n\nThe personal rule file [./personalRule.yar] doesn't exists" + "\x1b[0;m")
+        Handler()
+    rule = yara.compile(filepath="./personalRule.yar")
     allFiles = Target_Files(".")
     print("\n\n_______________________YARA PERSONAL RULE ANALYSIS_______________________\n")
     for pthFile in allFiles:
@@ -276,50 +301,112 @@ def Garbage_Collector(ver):
     Path.unlink(Path("./wp_" + ver + ".tar.gz"))
     Handler()
 
-def Scan_Wpvuldb(ver):
-    token = "YumCILXosAAvgLFDbAjK2z3iX4O41GS18exkg6KMM6U"
-    base = "https://wpvulndb.com/api/v3/wordpresses/"
+
+def Wpvuldb_Api(data):
+    # Will match in case data is a Wordpress version
+    regexVer = re.compile(r"^(\d{1,3}\.\d{0,3}\.{0,1}\d{0,3})")
+
+    if regexVer.match(data):
+        # The query is about Wordpress vulnerabilities
+        base = "https://wpvulndb.com/api/v3/wordpresses/"
+        url = base + data.replace(".", "")
+    else:
+        # The query is about Plugins vulnerabilities (a regex to match could be made but.. if "data" isn't
+        # a WP version is a plugin name(slug))
+        base = "https://wpvulndb.com/api/v3/plugins/"
+        url = base + data
+
+    token = ""
     authHeader = {"Authorization": "Token token=" + token}
-    url = base + ver.replace(".", "")
     apiReq = Request(url, headers=authHeader)
     siteResponse = urlReq(apiReq).read()
     jsonResponse = json.loads(siteResponse.decode("ISO-8859-1"))
-    versVuln = jsonResponse[ver]["vulnerabilities"]
+    listVuln = jsonResponse[data]["vulnerabilities"]
 
-    for vuln in versVuln:
+    for vuln in listVuln:
         vulName = vuln.get("title")
         vulType = vuln.get("vuln_type")
         vulDate = vuln.get("created_at")
         vulRef = vuln["references"].get("url")
         vulCVE = vuln["references"].get("cve")
-    
-        print("\n\nVulnerability name: " + vulName + "\nType: " + vulType + "\nDate: \
-" + vulDate + "\nReferences: " + str(vulRef) + "\nCVE: " + str(vulCVE))
+
+        print("\n\n\tVulnerability name: " + vulName + "\n\tType: " + vulType + "\n\tDate: \
+" + vulDate + "\n\tReferences: " + str(vulRef) + "\n\tCVE: " + str(vulCVE))
+
+
+def Plugins_Enum():
+    try:
+        pwd = "./wp-content/plugins"
+        plgFolders = [subDir for subDir in Path(
+            pwd).iterdir() if subDir.is_dir()]
+        rdmFile = "/README.txt"
+
+        for slug in plgFolders:
+            base = "https://api.wordpress.org/plugins/info/1.1/?action=query_plugins&request[search]="
+            url = base + slug.name
+            siteResponse = urlReq(url).read()
+            jsonResponse = json.loads(siteResponse.decode("ISO-8859-1"))
+
+            firstMatch = jsonResponse["plugins"][0]
+
+            plgName = firstMatch.get("name")
+            plgVersion = firstMatch.get("version")
+            plgAuthor = firstMatch.get("author_profile")
+            plgSite = firstMatch.get("homepage")
+            plgWPReq = firstMatch.get("requires")
+
+            if Path(str(slug) + rdmFile).is_file():
+                instVersion = verParser(rdmFile, slug)
+            elif Path(str(slug) + rdmFile.lower()).is_file():
+                instVersion = verParser(rdmFile.lower(), slug)
+
+            # Show info from Wordpress Api for each plugin parsed
+            print("\n\nPlugin name: " + plgName + "\x1b[1;31m" + "\nLatest version: " + plgVersion + "\nInstalled \
+version:" + instVersion + "\x1b[0;m" + "\nAuthor: " + plgAuthor + "\nSite: " + plgSite + "\nWordpress \
+version required: " + plgWPReq)
+
+            # Show a list of vulnerabilities store in wpvulndb for each plugin parsed
+            Wpvuldb_Api(slug.name)
+    except:
+        print("The directory \"wp-content\" doesn't exist, therefore plugins cannot be enumerated")
 
     Handler()
+
+
+def verParser(fileName, slug):
+    regEx = re.compile(r"(?!(?:#\n))(#|=)(\s{1})(\d+\.\d+\.\d+|\d+\.\d+|\d+)")
+
+    try:
+        with open(str(slug) + fileName, "r", encoding="ISO-8859-1") as rdmFile:
+            fullText = rdmFile.read()
+            plgVersion = regEx.search(fullText)
+            if plgVersion:
+                plgVersion = plgVersion.group(3)
+                return(plgVersion)
+    except EnvironmentError:
+        print("Plugin version cannot be determined.")
+
 
 def Handler(*args):
 
     print("\n\n1. Analyze Wordpress Core files hashes.\n2. Replace Wordpress \
 Core files from trust source.\n3. Find suspect variables and functions.\n4\
 . Download YARA Rules (latest).\n5. Malware analysis using YARA.\n6. WebShells \
-analysis using YARA.\n7. Load personal rule file and launch YARA.\n8. WPScan database \
-vulnerabilities scan.\n9. Exit\n\n")
+analysis using YARA.\n7. Load personal rule file and launch YARA.\n8. Show WORDPRESS \
+vulnerabilities affecting to installed Wordpress version. Source: WPVULNDB Database \
+(WPScan/Wordpressa).\n9. Show PLUGINS vulnerabilities. \
+Source: WPVULNDB Database (WPScan/Wordpressa).\n10. Exit\n\n")
 
     choice = input("Select an option: ")
 
     while True:
         if choice == "1":
             result = Target_Files(".")
-            badHashFiles, ver = Core_Analysis(result)
-            Handler(badHashFiles, ver)
+            Core_Analysis(result)
+            Handler()
         elif choice == "2":
-            if not args:
-                print("You must analyze your local installations first")
-                Handler()
-            else:
-                WP_CoreFiles_Replace(args[0], args[1])
-                Garbage_Collector(args[1])
+            WP_CoreFiles_Replace()
+            Garbage_Collector(ver)
         elif choice == "3":
             fullListFiles = Target_Files(".")
             Find_Suspect_Code(fullListFiles)
@@ -329,6 +416,7 @@ vulnerabilities scan.\n9. Exit\n\n")
             Handler()
         elif choice == "5":
             if Yara_Checker():
+                Yara_Rules_Adaptation()
                 Yara_Malware_Analysis()
             else:
                 print("You must download YARA first")
@@ -346,8 +434,12 @@ vulnerabilities scan.\n9. Exit\n\n")
                 print("You must download YARA first")
                 Handler()
         elif choice == "8":
-            Scan_Wpvuldb(args[1])
+            Wpvuldb_Api(ver)
+            Handler()
         elif choice == "9":
+            Plugins_Enum()
+            Handler()
+        elif choice == "10":
             print("\nBye bye!!\n")
             exit(0)
         break
@@ -355,6 +447,7 @@ vulnerabilities scan.\n9. Exit\n\n")
 
 def main():
     banner()
+    Answer_Ver()
     Handler()
 
 
